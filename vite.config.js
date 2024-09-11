c27e0445cdcadd203bebe4fb086ff04f245a62ba
Yashraj Maher <rajofearth@proton.me>
2024-09-11 12:39:29 +0530

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    sourcemap: true,
+  },
   publicDir: 'public',
   server: {
     middleware: (req, res, next) => {
