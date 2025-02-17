0c12c45758f1d5f84869148c0b0974408be903f2
rajofearth <rajofearth@proton.me>
2025-02-17 18:04:57 +0530

    Deploying to Vercel..

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "./components/theme-provider"
 function App() {
   return (
     <ThemeProvider>
-      <Router basename="/study-notes-hub">
+      <Router>
         <Routes>
           <Route path="/" element={<StudyNotesHubJsx />} />
           <Route path="/subject/:subjectId/semester/:semesterId/:noteType" element={<StudyNotesHubJsx />} />
