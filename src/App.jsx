cbebed55fa891b1f053f0d250979eb783ce214fb
rajofearth <rajofearth@proton.me>
2025-03-23 13:49:32 +0530

    chore: update site meta and add analytics
    **Changes:**
    
    - Added Vercel analytics to track site usage.
    - Updated site's `name`, `short_name`, `theme_color`, and `background_color` in the webmanifest file.
    - Updated content in Computer Science Semester 2 PDF.
    
    **Application:**
    
    - Vercel analytics provides valuable insights into user behavior and site traffic.
    - The manifest changes improve the app's presentation when installed or added to a home screen.
    - The updated PDF offers improved structure and content for Computer Science Semester 2.
    
    **Benefits:**
    
    - Data-driven insights from Vercel analytics can guide future development and improvements.
    - Enhanced user experience with better site metadata and presentation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { StudyNotesHubJsx } from "./components/study-notes-hub"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ThemeProvider } from "./components/theme-provider"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <ThemeProvider>
       <Router>
+        <Analytics/>
         <Routes>
           <Route path="/" element={<StudyNotesHubJsx />} />
           <Route path="/subject/:subjectId/semester/:semesterId/:noteType" element={<StudyNotesHubJsx />} />
